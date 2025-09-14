ea99f62d3c826ae997b5a7079cf2d71175a9d967
Jake Soenneker <jake@soenneker.com>
2025-09-14 14:21:58 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Initializing...
+﻿[![](https://img.shields.io/nuget/v/soenneker.quark.dtos.colors.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.quark.dtos.colors/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.quark.dtos.colors/publish-package.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.quark.dtos.colors/actions/workflows/publish-package.yml)
+[![](https://img.shields.io/nuget/dt/soenneker.quark.dtos.colors.svg?style=for-the-badge)](https://www.nuget.org/packages/soenneker.quark.dtos.colors/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Quark.Dtos.Colors
+### A flexible and dynamic color dto that allows for theming as well as custom values.
+
+## Installation
+
+```
+dotnet add package Soenneker.Quark.Dtos.Colors
+```
---
+++ src/Color.cs
@@ -0,0 +1,33 @@
+﻿using Soenneker.Quark.Enums.ColorTypes;
+
+namespace Soenneker.Quark.Dtos.Colors;
+
+/// <summary>
+/// A flexible and dynamic color dto that allows for theming as well as custom values.
+/// </summary>
+public readonly record struct Color
+{
+    public ColorType? Theme { get; }
+
+    public string? Css { get; }
+
+    public bool IsTheme => Theme is not null;
+
+    private Color(ColorType? theme, string? css)
+    {
+        Theme = theme;
+        Css = css;
+    }
+
+    public static Color FromTheme(ColorType theme) => new(theme, null);
+
+    public static Color FromCss(string css) => new(null, css);
+
+    public static implicit operator Color(ColorType theme) => FromTheme(theme);
+
+    public static implicit operator Color(string css) => FromCss(css);
+
+    public string? BuildClass(string prefix) => IsTheme ? $"{prefix}-{Theme!.Value}" : null;
+
+    public string? CssValueOrNull() => IsTheme ? null : Css;
+}
